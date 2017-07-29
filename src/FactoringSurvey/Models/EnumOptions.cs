f6cf8b23c47ebe043391766ca20b7c8948714818
czarekmart <czarekmart@gmail.com>
2017-07-29 13:57:06 -0700

    Working on model

---
+++ src/FactoringSurvey/Models/EnumOptions.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FactoringSurvey.Models
+{
+	public class EnumOption
+	{
+		public short Value { get; set; }
+		public string Description { get; set; }
+
+		public EnumOption(short value, string description)
+		{
+			Value = value;
+			Description = description;
+		}
+	}
+
+    public static class EnumOptions
+    {
+		public const short Unspecified = 0;
+
+		public const short GenderFemale = 1;
+		public const short GenderMale = 2;
+
+		public const short AgeTwenties = 20;
+		public const short AgeThirties = 30;
+		public const short AgeFourties = 40;
+		public const short AgeFifties = 50;
+		public const short AgeSixtyPlus = 60;
+
+		public static IEnumerable<EnumOption> Genders
+		{
+			get
+			{
+				return new[]
+				{
+					new EnumOption(Unspecified, "Unspecified"),
+					new EnumOption(GenderFemale, "Female"),
+					new EnumOption(GenderMale, "Male")
+				};
+			}
+		}
+		public static IEnumerable<EnumOption> Ages
+		{
+			get
+			{
+				return new[]
+				{
+					new EnumOption(Unspecified, "Unspecified"),
+					new EnumOption(AgeTwenties, "20-29"),
+					new EnumOption(AgeThirties, "30-39"),
+					new EnumOption(AgeFourties, "40-49"),
+					new EnumOption(AgeFifties, "50-59"),
+					new EnumOption(AgeSixtyPlus, "60+"),
+				};
+			}
+		}
+		public static string GetDescription(this IEnumerable<EnumOption> options, short value)
+		{
+			var opt = options.FirstOrDefault(o=>o.Value == value);
+			return opt == null ? string.Empty : opt.Description;
+		}
+	}
+}
--- src/FactoringSurvey/Models/SurveyResponse.cs
+++ src/FactoringSurvey/Models/SurveyResponse.cs
@@ -6,37 +6,58 @@ using System.Threading.Tasks;
 
 namespace FactoringSurvey.Models
 {
-	public enum Gender
-	{
-		Female = 0, Male = 1, Unspecified = 2
-	}
-	public enum Age
-	{
-		Twenties = 20,
-		Thirties = 30,
-		Fourties = 40,
-		Fifties = 50,
-		SixtyPlus = 60,
-		Unspecified = 0
-	}
-
 	public class SurveyResponse
     {
-		[Required(ErrorMessage ="Please enter your favorite pet animal")]
-		public string FavoritePet { get; set; }
-
 		[Required(ErrorMessage ="Please enter your name")]
+		[MaxLength(80)]
 		public string Name { get; set; }
 
 		[Required(ErrorMessage ="Please enter your country")]
+		[MaxLength(50)]
 		public string Country { get; set; }
 
-		[Required(ErrorMessage ="Please enter your age")]
-		public Age Age { get; set; }
+		[Required(ErrorMessage ="Please enter your email")]
+		[MaxLength(50)]
+		public string Email { get; set; }
 
-		public Gender? Gender { get; set; }
+		public short Gender { get; set; }   // one of GenderOptions
 
-		[MaxLength(240, ErrorMessage ="Enter max 240 characters")]
+		public short Age { get; set; }		// one of AgeOptions
+
+		[MaxLength(50)]
+		public string SchoolName { get; set; }
+
+		[MaxLength(50)]
+		public string MathQualification { get; set; } 
+
+		public int YearsOfTeachingMath { get; set; } 
+
+		public bool TeachingGrade6					{ get; set; }
+		public bool TeachingGrade7					{ get; set; }
+		public bool TeachingGrade8					{ get; set; }
+		public bool TeachingGrade9					{ get; set; }
+		public bool TeachingGrade10					{ get; set; }
+		public bool TeachingGrade11					{ get; set; }
+		public bool TeachingGrade12					{ get; set; }
+		public bool TeachingGradeCollege			{ get; set; }
+
+		public bool TeachingCourseBasic				{ get; set; }
+		public bool TeachingCoursePreAlgebra		{ get; set; }
+		public bool TeachingCourseAlgebra1			{ get; set; }
+		public bool TeachingCourseIntegrated		{ get; set; }
+		public bool TeachingCourseAlgebra2			{ get; set; }
+		public bool TeachingCoursePreCalculus		{ get; set; }
+		public bool TeachingCourseCalculus			{ get; set; }
+		public bool TeachingCourseProbability		{ get; set; }
+
+		[MaxLength(50)]
+		public string CourseOther			{ get; set; }
+
+		[MaxLength(240)]
 		public string Comment { get; set; }
+
+
+
+
 	}
 }
